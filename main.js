2bfe6ce46133ee55f9151affb9a61f86050a3651
ZZZoucy <79960164+ZZZoucy@users.noreply.github.com>
2021-05-07 20:33:29 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+var div1 = document.createElement('div');
+div1.className = 'demo';
+div1.style.top = 'auto';
+
+document.body.appendChild(div1);
+
+var dragging = false;
+
+var lastX;
+var lastY;
+
+div1.onmousedown = function (e) {
+    lastX = e.clientX;
+    lastY = e.clientY;
+    dragging = true;
+}
+
+document.onmousemove = function (e) {
+    if (dragging === true) {
+        var deltaX = e.clientX - lastX;
+        var deltaY = e.clientY - lastY;
+        var top = parseInt(div1.style.top) || 0;
+        var left = parseInt(div1.style.left) || 0;
+        div1.style.top = top + deltaY + 'px';
+        div.style.left = left + deltaY + 'px';
+        lastX = e.clientX;
+        lastY = e.clientY;
+    }
+}
+
+document.onmouseup = function () {
+    dragging = false;
+}
